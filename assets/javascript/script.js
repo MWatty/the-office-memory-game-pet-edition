198efa5e2a523991e6eb67fc8e034fceb9967a94
MWatty <maura1984@icloud.com>
2021-03-22 19:33:27 +0000

    amended game html to create spacing between the word flips and the flip counter

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
-class OfficeAudio {
+class officeAudio {
     constructor(){
-        this.flipSound = new Audio('assets/audio/woof.wav');
+        this.flipSound = new Audio('assets/audio/flip.wav');
         this.matchSound = new Audio('assets/audio/win.wav');
         this.winSound = new Audio('assets/audio/cheer.wav');
         this.loseSound = new Audio('assets/audio/lose.wav');
@@ -19,25 +19,54 @@ class OfficeAudio {
     }
 }
 
+class theOffice {
+    constructor(totalTime, card) {
+        this.cardArray = card;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.countdown = document.getElementById('time-left');
+        this.counter = document.getElementById('flips');
+        this.officeAudio = new officeAudio();
+    }
+    startGame() {
+        this.cardsToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCard = [];
+        this.busy = true;
+       
+    }
+    flipCards(cards){
+        if(this.canflipCards(cards)) {
+            this.officeAudio.flip();
+            this.totalClicks++;
+            this.counter.innerText = this.totalClicks;
+            cards.classList.add('visible');
+        }
+    }
 
-
-
+    canflipCards(cards){
+        return true;
+    }
+}
 
 
 
 function ready() {
     let gametext = Array.from(document.getElementsByClassName('gameplay-text'));
     let card = Array.from(document.getElementsByClassName('cards'));
+    let game = new theOffice(60, card);
+
 
     gametext.forEach(gameplay => {
         gameplay.addEventListener('click', () => {
             gameplay.classList.remove('visible');
-            //need to add function
+            game.startGame();
         });
     });
     card.forEach(cards => {
         cards.addEventListener('click', () => {
-            //need to add function
+          game.flipCards(cards);
         });
     });
 }
