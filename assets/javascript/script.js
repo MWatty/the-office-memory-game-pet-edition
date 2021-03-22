5764ec85bcd65d1efa9bf6e90b3941ffd4c1362c
MWatty <maura1984@icloud.com>
2021-03-22 17:34:50 +0000

    created audio class

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,29 @@
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
+class OfficeAudio {
+    constructor(){
+        this.flipSound = new Audio('assets/audio/woof.wav');
+        this.matchSound = new Audio('assets/audio/win.wav');
+        this.winSound = new Audio('assets/audio/cheer.wav');
+        this.loseSound = new Audio('assets/audio/lose.wav');
+    }
+    flip(){
+        this.flipSound.play();
+    }
+    match(){
+        this.matchSound.play();
+    }
+    win(){
+        this.winSound.play();
+    }
+    lose(){
+        this.loseSound.play();
+    }
 }
+
+
+
+
+
+
 
 function ready() {
     let gametext = Array.from(document.getElementsByClassName('gameplay-text'));
@@ -11,6 +32,18 @@ function ready() {
     gametext.forEach(gameplay => {
         gameplay.addEventListener('click', () => {
             gameplay.classList.remove('visible');
+            //need to add function
         });
     });
+    card.forEach(cards => {
+        cards.addEventListener('click', () => {
+            //need to add function
+        });
+    });
+}
+
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
 }
