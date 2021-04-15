cece7f2b6a2f8e3c665df459ab51a9effd1ddece
MWatty <maura1984@icloud.com>
2021-04-15 13:55:00 +0000

    added some explanatory comments to JavaScript file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,153 +1,148 @@
+//created to add sound effects to provide an interactive element to the game 
 class OfficeAudio {
-    constructor(){
+    constructor() {
         this.flipSound = new Audio('assets/audio/flip.wav');
         this.matchSound = new Audio('assets/audio/win.wav');
         this.winSound = new Audio('assets/audio/cheer.wav');
         this.loseSound = new Audio('assets/audio/lose.wav');
     }
-    flip(){
+    flip() {
         this.flipSound.play();
     }
-    match(){
+    match() {
         this.matchSound.play();
     }
-    win(){
-        //need to change this sounds its too long 
+    win() {
         this.winSound.play();
     }
-    lose(){
+    lose() {
         this.loseSound.play();
     }
 }
 
 class TheOffice {
     constructor(totalTime, card) {
-        this.cardArray = card;
-        this.totalTime = totalTime;
-        this.timeRemaining = totalTime;
-        this.countDown = document.getElementById('time-left');
-        this.counter = document.getElementById('flips');
-        this.OfficeAudio = new OfficeAudio();
+        this.cardArray = card;//property of the object which is set from the constructor 
+        this.totalTime = totalTime;//property of the object which is set from the constructor 
+        this.timeRemaining = totalTime;//whatever the time remaining is at any point given throughout the game
+        this.countDown = document.getElementById('time-left');//the actual time pulled from the DOM
+        this.counter = document.getElementById('flips');//flip counter pulled from the DOM 
+        this.OfficeAudio = new OfficeAudio();//Office Audio that belongs to this particular game object 
     }
 
 
     startGame() {
-        this.cardsToCheck = null;
-        this.totalClicks = 0;
-        this.timeRemaining = this.totalTime;
-        this.matchedCard = [];
-        this.busy = true;
+        this.cardsToCheck = null;//when you first start game no cards to check as no cards flipped, when you flip one it becomes cardToCheck
+        this.totalClicks = 0;//each time a new game is started flip counter is equal to 0
+        this.timeRemaining = this.totalTime;//time resets each time a new game is started 
+        this.matchedCard = [];//all the matched cards we get whilst playing go into this array, check against cardArray for victory
+        this.busy = true;//is there a scenario occuring whereby the game is busy 
         setTimeout(() => {
-        this.shuffleCards();
-        this.timer = this.startTimer();
-        this.busy = false;
-    }, 500);
+            this.shuffleCards();
+            this.timer = this.startTimer();
+            this.busy = false;
+        }, 500);
         this.turnCardBack();
-       this.countDown.innerText = this.timeRemaining;
+        this.countDown.innerText = this.timeRemaining;
         this.counter.innerText = this.totalClicks;
-}
+    }
 
 
     startTimer() {
         return setInterval(() => {
-        this.timeRemaining--;
-        this.countDown.innerText = this.timeRemaining;
-        if(this.timeRemaining === 0)
-        this.gameOver();
+            this.timeRemaining--;
+            this.countDown.innerText = this.timeRemaining;
+            if (this.timeRemaining === 0)
+                this.gameOver();
         }, 1000);
     }
 
     //create a function to pause the game 
     //create a function to reset the game 
 
-    
-    flipCards(cards){
-        if(this.canflipCards(cards)) {
+
+    flipCards(cards) {
+        if (this.canflipCards(cards)) {
             this.OfficeAudio.flip();
             this.totalClicks++;
             this.counter.innerText = this.totalClicks;
             cards.classList.add('visible');
-                        
-            if(this.cardsToCheck) {
-            this.areCardsMatched(cards);
+
+            if (this.cardsToCheck) {
+                this.areCardsMatched(cards);
             } else {
-            this.cardsToCheck = cards;
+                this.cardsToCheck = cards;
+            }
         }
     }
+
+    areCardsMatched(cards) {
+        if (this.whatTypeCard(cards) === this.whatTypeCard(this.cardsToCheck)) {
+            this.cardsMatched(cards, this.cardsToCheck);
+            this.cardsToCheck = null;
+        }
+        else {
+            this.cardsNoMatch(cards, this.cardsToCheck);
+            this.cardsToCheck = null;
+        }
     }
 
-    //areCardsMatched(cards){
-     //if(this.whatTypeCard(cards) === this.whatTypeCard(this.cardsToCheck)) 
-    // this.cardsMatched(cards, this.cardsToCheck);
-    //  else 
-    // this.cardsNoMatch(cards, this.cardsToCheck);
-    // this.cardsToCheck = null;
-    //}
 
-areCardsMatched(cards) {
-if (this.whatTypeCard(cards) === this.whatTypeCard(this.cardsToCheck)) {
-this.cardsMatched(cards, this.cardsToCheck);
-//this.cardsToCheck = null;
-}
-else {
-this.cardsNoMatch(cards, this.cardsToCheck);
-this.cardsToCheck = null;
-}
-}
 
-    
-
-    whatTypeCard(cards){
+    whatTypeCard(cards) {
         return cards.getElementsByClassName('dog-card')[0].src;
     }
 
-    cardsMatched(cards1, cards2){
+    cardsMatched(cards1, cards2) {
         this.matchedCard.push(cards1);
         this.matchedCard.push(cards2);
         this.OfficeAudio.match();
-        if(this.matchedCard.length === this.cardArray.length)
-        this.winner();
+        if (this.matchedCard.length === this.cardArray.length)
+            this.winner();
     }
 
 
-    cardsNoMatch(cards1, cards2){
-      this.busy = true;
-      setTimeout(() => {
-          cards1.classList.remove('visible');
-          cards2.classList.remove('visible');
-          this.busy = false;      
-    } ,1000);
-}
-
-
-gameOver() {
-   clearInterval(this.timer);
-   this.OfficeAudio.lose();
-    document.getElementById('game-over-text').classList.add('visible');
-}
-
-winner(){
-    clearInterval(this.timer);
-    this.OfficeAudio.win();
-    document.getElementById('winner-text').classList.add('visible');
-}
-
-turnCardBack(){
-    this.cardArray.forEach(cards => {
-    cards.classList.remove('visible');
-    });
+    cardsNoMatch(cards1, cards2) {
+        this.busy = true;
+        setTimeout(() => {
+            cards1.classList.remove('visible');
+            cards2.classList.remove('visible');
+            this.busy = false;
+        }, 1000);
     }
 
-//Fisher Yates shuffle algorothim https://medium.com/@oldwestaction/randomness-is-hard-e085decbcbb2
-    shuffleCards(){
-        for(let i = this.cardArray.length -1; i > 0; i--){
-            let randomInt = Math.floor(Math.random() * (i+1));
+
+    gameOver() {
+        clearInterval(this.timer);
+        this.OfficeAudio.lose();
+        document.getElementById('game-over-text').classList.add('visible');
+    }
+
+    winner() {
+        clearInterval(this.timer);
+        this.OfficeAudio.win();
+        document.getElementById('winner-text').classList.add('visible');
+    }
+
+    turnCardBack() {
+        this.cardArray.forEach(cards => {
+            cards.classList.remove('visible');
+        });
+    }
+
+//Fisher Yates shuffle algorothim used to shuffle cards https://medium.com/@oldwestaction/randomness-is-hard-e085decbcbb2
+    shuffleCards() {
+        for (let i = this.cardArray.length - 1; i > 0; i--) {
+            let randomInt = Math.floor(Math.random() * (i + 1));
             this.cardArray[randomInt].style.order = i;
             this.cardArray[i].style.order = randomInt;
         }
     }
-
-    canflipCards(cards){
+    
+//scenarios whereby user cannot flip a card, game busy, clicking on a card that is already matched, clicking on card that is already flipped waiting card to check 
+//creates a boolean, if all 3 values are false this will return true 
+// if this returns true user can flip the card
+canflipCards(cards) {
         return !this.busy && !this.matchedCard.includes(cards) && cards !== this.cardsToCheck;
     }
 }
@@ -159,7 +154,6 @@ function ready() {
     const timeAllowed = 60;
     let game = new TheOffice(timeAllowed, card);
 
-
     gametext.forEach(gameplay => {
         gameplay.addEventListener('click', () => {
             gameplay.classList.remove('visible');
@@ -168,12 +162,14 @@ function ready() {
     });
     card.forEach(cards => {
         cards.addEventListener('click', () => {
-          game.flipCards(cards);
+            game.flipCards(cards);
         });
     });
 }
 
-if(document.readyState === 'loading') {
+//if the HTML page is not loaded put an EventListener on the DOM that says when it is loaded call the ready function
+//or else if it is loaded it will call the ready function
+if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', ready());
 } else {
     ready();
